5c3e84219e296ef2b0845fec7633ba5e544146d4
Bryan Jurish <jurish@bbaw.de>
2015-07-21 13:23:17 +0000

    + updated to v0.07.007 : allow ddc queries without primary targets (=1), for 'subcorpus comparison'
    git-svn-id: file:///home/svn/dev/DiaColloDB/trunk@15016 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -33,7 +33,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = "0.07.006";
+our $VERSION = "0.07.007";
 our @ISA = qw(DiaColloDB::Client);
 
 ## $PGOOD_DEFAULT
