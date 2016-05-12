0c68f07907d53d41e812b73c996e364e90a760d3
Bryan Jurish <jurish@bbaw.de>
2016-05-12 14:58:54 +0000

    + updated to v0.10.001 for cpan release
    git-svn-id: file:///home/svn/dev/DiaColloDB/trunk@16123 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -41,7 +41,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = "0.10.000";
+our $VERSION = "0.10.001";
 our @ISA = qw(DiaColloDB::Client);
 
 ## $PGOOD_DEFAULT
