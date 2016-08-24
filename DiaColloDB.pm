09a4679daad8c59c247bc0ad3dd9c0a5b347f456
Bryan Jurish <jurish@bbaw.de>
2016-08-24 12:22:13 +0000

    + removed bogus v0.10.009 version
    git-svn-id: file:///home/svn/dev/DiaColloDB/trunk@16415 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -41,7 +41,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = "0.10.009";
+our $VERSION = "0.10.008";
 our @ISA = qw(DiaColloDB::Client);
 
 ## $PGOOD_DEFAULT
