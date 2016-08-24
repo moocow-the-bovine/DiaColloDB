b877e45f0a167fc9b776ad562668e589d0784980
Bryan Jurish <jurish@bbaw.de>
2016-08-24 12:12:49 +0000

    v0.10.008: 	* merged in debugging changes from v0.10.004 debugging branch (_v0.10.004_0[123]) 	  - conditionally enabled by new dcdb-create.perl -debug option 	* added Utils::fh_flush() and Utils::fh_reopen() methods 	  - fh_reopen() should simulate flush() even on systems which don't support flush() 	* updated Persistent subclasses to call fh_reopen() from their flush() methods: 	  - EnumFile(+FixedLen +FixedMap +MMap), MultiMapFile(+MMap), PackedFile(+MMap)
    git-svn-id: file:///home/svn/dev/DiaColloDB/trunk@16410 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -41,7 +41,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = "0.10.007";
+our $VERSION = "0.10.008";
 our @ISA = qw(DiaColloDB::Client);
 
 ## $PGOOD_DEFAULT
