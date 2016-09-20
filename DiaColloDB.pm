c781f8bf2bd3be91028705e20e84bb6025d953b1
Bryan Jurish <jurish@bbaw.de>
2016-09-20 07:52:45 +0000

    + updated to v0.11.001
    git-svn-id: file:///home/svn/dev/DiaColloDB/branches/v0.10.009+threaded-list-client@16560 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -41,7 +41,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = "0.11.000";
+our $VERSION = "0.11.001";
 our @ISA = qw(DiaColloDB::Client);
 
 ## $PGOOD_DEFAULT
