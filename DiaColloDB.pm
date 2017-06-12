6341202dcd6f43a9a7220bb2333f7f571bc3d83e
Bryan Jurish <jurish@bbaw.de>
2017-06-12 12:51:57 +0000

    * list-client fix for redundant extend()
    git-svn-id: file:///home/svn/dev/DiaColloDB/trunk@26900 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -41,7 +41,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = "0.12.006";
+our $VERSION = "0.12.007";
 our @ISA = qw(DiaColloDB::Client);
 
 ## $PGOOD_DEFAULT
