6178325c50d54b34f6fb66b003675217a8fc00a2
Bryan Jurish <jurish@bbaw.de>
2020-01-16 13:13:19 +0000

    + updated to v0.12.012_03
    git-svn-id: file:///home/svn/dev/DiaColloDB/trunk@31381 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -44,7 +44,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = "0.12.012_01";
+our $VERSION = "0.12.012_03";
 our @ISA = qw(DiaColloDB::Client);
 
 ## $TDF_MGOOD_DEFAULT
