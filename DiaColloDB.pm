e304c976461e922eecb8038a0784c9057868f8ed
Bryan Jurish <jurish@bbaw.de>
2020-03-10 11:14:49 +0000

    + DiaColloDB.pm: updated TDF_MGOOD_DEFAULT (added country|region|party|role)
    git-svn-id: file:///home/svn/dev/DiaColloDB/trunk@31782 4a515de8-beb3-11dc-84d6-2b654028afac

--- DiaColloDB.pm
+++ DiaColloDB.pm
@@ -50,7 +50,7 @@ our @ISA = qw(DiaColloDB::Client);
 ## $TDF_MGOOD_DEFAULT
 ##  + default positive meta-field regex for document parsing (tdf only)
 ##  + don't use qr// here, since Storable doesn't like pre-compiled Regexps
-our $TDF_MGOOD_DEFAULT = q/^(?:author|pnd|title|basename|collection|flags|textClass|genre)$/;
+our $TDF_MGOOD_DEFAULT = q/^(?:author|pnd|title|basename|collection|flags|textClass|genre|country|region|party|role)$/;
 
 ## $TDF_MBAD_DEFAULT
 ##  + default negative meta-field regex for document parsing (tdf only)
